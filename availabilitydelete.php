af08fd7c4fa8e66a4c3491bbc144cc19223c1a53
ScCollins21 <spencer1.collins1@gmail.com>
2025-04-28 20:10:54 -0500

    almost working delete

--- availabilitydelete.php
+++ availabilitydelete.php
@@ -5,6 +5,37 @@
     if(isset($_GET['availabilityid'])){
         $availabilityid = $_GET['availabilityid'];
 
+        $newsql = "SELECT * FROM appointment_availability WHERE availabilityid = ?";
+
+        $appdeletesql = "DELETE FROM appointments WHERE availabilityid = ?";
+
+        if ($stmt = $conn->prepare($newsql)) {
+            $stmt->bind_param("i", $availabilityid);
+            
+            $stmt->execute();
+
+            if($stmt){
+                $result = $stmt->get_result()->fetch_assoc();
+                $aptid = $result['appointmentid'];
+            }
+
+        
+        }else{
+            echo "Error preparing statement: " . $conn->error;
+        }
+
+        if ($stmt = $conn->prepare($appdeletesql)) {
+            $stmt->bind_param("i", $aptid);
+            
+            if ($stmt->execute()) {
+            } else {
+                echo "Error deleting record: " . $conn->error;
+            }
+            $stmt->close();
+        }else{
+            echo "Error preparing statement: " . $conn->error;
+        }
+
         $sql = "DELETE FROM availability WHERE availabilityid = ?";
 
         if ($stmt = $conn->prepare($sql)) {
